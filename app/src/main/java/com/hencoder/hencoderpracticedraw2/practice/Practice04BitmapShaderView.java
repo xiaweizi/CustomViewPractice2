23c5564df46609686011c52cfa663c5513f99c0c
xiaweizi <1012126908@qq.com>
2018-09-05 21:28:31 +0800

    [dev] 自定义 View BitmapShader

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
@@ -1,11 +1,18 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice04BitmapShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -25,6 +32,13 @@ public class Practice04BitmapShaderView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 BitmapShader
         // Bitmap: R.drawable.batman
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        Matrix matrix = new Matrix();
+        matrix.postScale(2, 2);
+        Bitmap newbm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix,true);
+
+        paint.setShader(new BitmapShader(newbm, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+
     }
 
     @Override
